ae0015b5e58e53490d4fe96fbcb9cf52d80d5cb4
Helena Kopp <helena.mkopp@gmail.com>
2020-07-07 10:27:46 -0300

    final do projeto

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const alunosDaTurmaA = [
     {
         nome: 'Helena',
-        nota: 9.8
+        nota: 3.1
     },
-    
+
     {
         nome: 'Paula',
         nota: 8.9
     },
-    
+
     {
         nome: 'Gabriela',
-        nota: 9.3
+        nota: 4.7
     },
     {
         nome: 'Isis',
@@ -34,14 +34,14 @@ const alunosDaTurmaB = [
     },
     {
         nome: 'Carol',
-        nota: 10.0
+        nota: 3.1
     }
 ]
 
 
 function calculaMedia(alunos) {
     let soma = 0;
-    for(let i = 0; i < alunos.length; i++) {
+    for (let i = 0; i < alunos.length; i++) {
         soma = soma + alunos[i].nota
     }
 
@@ -54,13 +54,36 @@ const media1 = calculaMedia(alunosDaTurmaA)
 const media2 = calculaMedia(alunosDaTurmaB)
 
 
-function enviaMensagem(media, turma){
-    if(media > 5){
+function enviaMensagem(media, turma) {
+    if (media > 5) {
         console.log(`A média da ${turma} foi de ${media} Parabens`)
-    }else{
+    } else {
         console.log(`A ${turma} obteve média ${media} , ou seja, não atingiram o objetivo.`)
     }
 }
 
 enviaMensagem(media1, 'Turma A')
 enviaMensagem(media2, 'Turma B')
+
+function marcarComoReprovado(aluno) {
+    aluno.reprovado = false;
+    if (aluno.nota < 5) {
+        aluno.reprovado = true;
+    }
+}
+
+function enviarMensagemReprovado(aluno) {
+    if (aluno.reprovado) {
+        console.log(`O aluno ${aluno.nome} está reprovado!`)
+    }
+}
+
+function alunoReprovado(alunos) {
+    for (let aluno of alunos) {
+        marcarComoReprovado(aluno);
+        enviarMensagemReprovado(aluno);
+    }
+}
+
+alunoReprovado(alunosDaTurmaA)
+alunoReprovado(alunosDaTurmaB)
